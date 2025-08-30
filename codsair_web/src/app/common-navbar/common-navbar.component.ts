7e8ee387f25b3ecd99906c7d7c1c6ffa9d685965
Anamika <104248445+anamika1111@users.noreply.github.com>
2025-08-30 15:58:40 +0530

    30-08-2025

--- codsair_web/src/app/common-navbar/common-navbar.component.ts
+++ codsair_web/src/app/common-navbar/common-navbar.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-common-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
   templateUrl: './common-navbar.component.html',
   styleUrl: './common-navbar.component.css'
 })
 export class CommonNavbarComponent {
 
+
+
+  constructor(private router: Router) { }
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  navigate(path: string) {
+    this.router.navigate([path]);
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar') && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
 }
--- codsair_web/src/app/home/home.component.ts
+++ codsair_web/src/app/home/home.component.ts
@@ -12,4 +12,46 @@ import { CommonNavbarComponent } from "../common-navbar/common-navbar.component"
 })
 export class HomeComponent {
 
+
+  words: string[] = ["Innovation", "Creativity", "Technology"]; 
+  wordIndex: number = 0;
+  charIndex: number = 0;
+
+  ngAfterViewInit() {
+    this.typingEffect();
+  }
+
+  typingEffect() {
+    const typingElement = document.getElementById("typing");
+
+    if (typingElement) {
+      let currentWord = this.words[this.wordIndex];
+
+      if (this.charIndex < currentWord.length) {
+        typingElement.innerHTML += currentWord.charAt(this.charIndex);
+        this.charIndex++;
+        setTimeout(() => this.typingEffect(), 120);
+      } else {
+        // wait before erasing
+        setTimeout(() => this.eraseEffect(), 1500);
+      }
+    }
+  }
+
+  eraseEffect() {
+    const typingElement = document.getElementById("typing");
+
+    if (typingElement) {
+      if (this.charIndex > 0) {
+        typingElement.innerHTML = typingElement.innerHTML.slice(0, -1);
+        this.charIndex--;
+        setTimeout(() => this.eraseEffect(), 80);
+      } else {
+        // move to next word
+        this.wordIndex = (this.wordIndex + 1) % this.words.length;
+        setTimeout(() => this.typingEffect(), 300);
+      }
+    }
+  }
+
 }
